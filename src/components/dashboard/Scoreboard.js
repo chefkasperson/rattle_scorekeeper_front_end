b3ef518918f0029f5e8773eb87857e40c14d20f6
github username <github email address>
2020-05-17 04:15:26 -0700

    styling to scoreboard

--- src/components/dashboard/Scoreboard.js
+++ src/components/dashboard/Scoreboard.js
@@ -4,26 +4,28 @@ function Scoreboard(props) {
   const game = props.game
   return (
     <div className='container section'>
-      <div className="card-content white z-depth-3">
-        <h3 className='center'>Scoreboard</h3>
-        <table className="striped">
-          <thead>
-            <tr>
-              <th>{(game && game.player_1.name)}</th>
-              <th>{(game && game.player_2.name)}</th>
-              <th>{(game && game.player_3.name)}</th>
-            </tr>
-          </thead>
+      <div className="card">
+        <div className="card-content white z-depth-3">
+          <h3 className='center grey-text text-darken-3'>Scoreboard</h3>
+          <table className="striped grey-text text-darken-3">
+            <thead>
+              <tr>
+                <th>{(game && game.player_1.name)}</th>
+                <th>{(game && game.player_2.name)}</th>
+                <th>{(game && game.player_3.name)}</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            <tr>
-              <td>{(game && game.p1_score)}</td>
-              <td>{(game && game.p2_score)}</td>
-              <td>{(game && game.p3_score)}</td>
-            </tr>
-          
-          </tbody>
-        </table>
+            <tbody>
+              <tr>
+                <td>{(game && game.p1_score)}</td>
+                <td>{(game && game.p2_score)}</td>
+                <td>{(game && game.p3_score)}</td>
+              </tr>
+            
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
